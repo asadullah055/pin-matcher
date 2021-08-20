34ecc78e8349933a49308b2de621d8bd6549d4f5
asadullah055 <asadullahahmed01@gmail.com>
2021-08-20 09:00:03 +0600

    first commit

---
+++ pin-matcher-master/pin.js
@@ -0,0 +1,46 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + ''
+    if (pinString.length == 4) {
+        return pin;
+    }
+    else {
+        return getPin()
+    }
+}
+
+function generatePin() {
+    const pin = getPin()
+    document.getElementById('display-pin').value = pin
+}
+
+document.getElementById('calator-keypad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('type-numbers')
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = ''
+        }
+    }
+    else {
+        const previousNumber = calcInput.value
+        const newNumber = previousNumber + number
+        calcInput.value = newNumber
+    }
+
+})
+
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value
+    const type = document.getElementById('type-numbers').value
+    const successMassage = document.getElementById('notify-success')
+    const faildMassage = document.getElementById('notify-faild')
+    if (pin == type) {
+        successMassage.style.display = 'block'
+        faildMassage.style.display = 'none'
+    }
+    else {
+        faildMassage.style.display = 'block'
+        successMassage.style.display = 'none'
+    }
+}
